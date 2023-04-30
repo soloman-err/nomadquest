531781ab17ee6628ba9f57403994be07e47d3721
soloman-err <soloman.err@gmail.com>
2023-05-01 02:07:40 +0600

    Layout implemented & added tailwindcss

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Shared/Header";
+import Home from "../Pages/Home";
+
+const Root = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/Shared/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Destination</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
